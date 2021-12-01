44f03cc7140c6b71a29dfee8a46306bf327d73f3
bitraylee <anshumohanta@hotmail.com>
2021-12-01 13:31:54 +0530

    made the email working

--- sendmail.php
+++ sendmail.php
@@ -1,2 +1,11 @@
 <?php
-mail('venominside12@gmail.com', 'Test Subject', 'Hello World', 'From: ampele2000@yahoo.com');
+$to = "user2@randomxkcd.com";
+$subject = "diamond";
+$message = "diamond";
+$from = "user1@randomxkcd.com";
+$headers = "From: $from";
+if (mail($to, $subject, $message, $headers)) {
+   echo "Mail Sent.";
+} else {
+   echo "Mail is not sent.";
+}
